722d414d60f87e563fc8439630191d5502824b84
michael <you@example.com>
2021-10-10 20:47:23 +0300

    added .chezmoiignore

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "teeth"
-      user-mail-address "funwithteeth@protonmail.com")
+(setq user-full-name "kou"
+      user-mail-address "mikeprokofyev@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -19,18 +19,13 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "monospace" :size 20 :weight 'semi-light)
+(setq doom-font (font-spec :family "Iosevka SS15 Extended" :size 20 )
        doom-variable-pitch-font (font-spec :family "sans" :size 21))
+
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -53,8 +48,65 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-;;
-;; Use html-mode for go .tmpl files
+
+
+(add-hook 'markdown-mode-hook (lambda () (writeroom-mode +1)))
+(remove-hook 'markdown-mode-hook (lambda () (variable-pitch-mode)))
+
+(with-eval-after-load 'lsp-mode
+  (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\Library\\'")
+  (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\Temp\\'")
+  ;; or
+  ;; (add-to-list 'lsp-file-watch-ignored-files "[/\\\\]\\.my-files\\'")
+  (setq lsp-ui-sideline-show-code-actions nil)
+(setq lsp-ui-doc-enable nil)
+(setq lsp-lens-enable t)
+  )
+
+; Iterate through CamelCase words
+(global-subword-mode 1)
+
+
+(defadvice! my-evil-delete-default-to-black-hole-a (fn beg end &optional type register yank-handler)
+  "Advise `evil-delete' to set default REGISTER to the black hole register."
+  :around #'evil-delete
+  (unless register (setq register ?_))
+  (funcall fn beg end type register yank-handler))
+
+;; org
+(setq org-directory "~/Dropbox/writing/")
+(defun kou/org-reformat-buffer ()
+  (interactive)
+  (when (y-or-n-p "Really format current buffer? ")
+    (let ((document (org-element-interpret-data (org-element-parse-buffer))))
+      (erase-buffer)
+      (insert document)
+      (goto-char (point-min)))))
+
+;; (package! org-download)
+(defun kou/org-download-paste-clipboard (&optional use-default-filename)
+  (interactive "P")
+  (require 'org-download)
+  (let ((file
+         (if (not use-default-filename)
+             (read-string (format "Filename [%s]: "
+                                  org-download-screenshot-basename)
+                          nil nil org-download-screenshot-basename)
+           nil)))
+    (org-download-clipboard file)))
+
+(after! org
+  (setq org-download-method 'directory)
+  (setq org-download-image-dir "images")
+  (setq org-download-heading-lvl nil)
+  (setq org-download-timestamp "%Y%m%d-%H%M%S_")
+  (setq org-image-actual-width 300)
+  (map! :map org-mode-map
+        "C-c l a y" #'kou/org-download-paste-clipboard
+        "C-i p" #'kou/org-download-paste-clipboard))
+
+
+;; Go lang things
 (setq auto-mode-alist
       (append '((".*\\.tmpl\\'" . web-mode))
               auto-mode-alist))
@@ -63,5 +115,3 @@
         ("go"    . "\\.tmpl\\'")
         )
 )
-(add-hook 'markdown-mode-hook (lambda () (writeroom-mode +1)))
-(remove-hook 'markdown-mode-hook (lambda () (variable-pitch-mode)))
